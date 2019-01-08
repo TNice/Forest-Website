0ce2b133767aea2bf705b1dba9b9a6d414083348
TNice <tylernicewarner@yahoo.com>
2019-01-08 13:54:26 -0500

    Fixed Error From Merge

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-document.getElementById("species").onmouseover = function () {
-    document.getElementById("speciesNav").style.visibility = "visible";
-    console.log("test");
-};
+/*Submenu Ids:
+Species = 0
+Disturbance = 1
+*/
+
+function OpenSubMenu(id) {
+    var tagId = "sub-menu-" + id;
+    console.log(tagId);
+    document.getElementById(tagId).style.width = "10vw";
+}
+
+function CloseSubMenu(id) {
+    var tagId = "sub-menu-" + id;
+    console.log(tagId);
+    document.getElementById(tagId).style.width = "0";
+}
