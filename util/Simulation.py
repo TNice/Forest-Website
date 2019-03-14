37554645adca35ff38410aa391f341554b7b09e7
TNice <tylernicewarner@yahoo.com>
2019-03-14 10:47:31 -0500

    Update Simulation.py
    refactoring simulation sthon script

--- util/Simulation.py
+++ util/Simulation.py
@@ -5,25 +5,68 @@ import RemoveFiles as rf
 import python.FileParser as FileParser
 
 timeInc = 2 #timestep for simulation
-regionArea = 900 #region area of our single cell test space (30m x 30m)
-speciesArray = [] #array of species
-numTree = 0 #number of trees
 
-#variables for matplotlib graphs
-pieFile = "graphs/pie.png"
-barFile = "graphs/bar.png"
-pieLabels = 'Pine', 'Cedar', 'Red Oak', 'White Oak', 'Hickory', 'Maple', 'Elm','Pseudosp'
-barLabels = 'Pine', 'Cedar', 'Red Oak', 'White Oak', 'Hickory', 'Maple', 'Elm','Pseudosp', 'Region'
-values = []
-explode = (0, 0, 0, 0, 0, 0, 0, 0)
-N = 9
-GSOValues = []
-ind = np.arange(N)
-width = 0.5
-#end matplotlib
+species = []
 
-class Species:
-    trees = [] #array of all trees in species 
+class Region:
+    def __init__(self, area, speciesNum, treeNum)
+        self.regionArea = area
+        self.speciesArray = GenerateSpecies(self, speciesNum)
+        self.numTree = treeNum
+        self.trees = GenerateTrees()
+
+    def GenerateSpecies(self, num):
+        array = []
+        for i in range(1, num):
+            sindex = random.randint(0, num - 1)
+            array.append(species[sindex])
+        return array
+    
+    def GenerateTrees(self):
+        array = []
+        for s in self.speciesArray:
+            numTreeInSpecies = random.randint(500, 900)
+            for i in range(numTreeInSpecies):
+                array.append(Tree(s, random.randint(0, int(s.Longevity)//timeInc)))
+
+        return array
+
+    #Calculates The Number of Trees in a diameter group (currently using the age cohort to group since DBH is determined by age)
+    def NumberTreesInDiameterGroup(self, species, age, timestep):
+        num = 0;
+        for tree in trees:
+            if tree.species == species:
+                if tree.AgeCohort == age:
+                    num += 1
+
+        return self.DBH(age, timestep), num #returns the DBH of the group along with the number of trees in the group
+
+    #DBH for species according to user guide formula
+    def DBH(self, species, ageCohort, timestep, a = 1, k = 1.5):
+        age = ageCohort * timestep
+        #end constants
+        return int(species.MaxDiameter) * math.exp(-a * math.exp(-k * (age / int(species.Longevity))))
+
+    #GSO for a species according to user guide formula
+    def GrowSpaceSpecies(self, species, timestep):
+        result = 0
+        for i in range(1, int(species.Longevity)//timestep):
+            dbh, numberTrees = self.NumberTreesInDiameterGroup(i, species, timestep)
+            result += ((dbh / 10) ** 1.605) * numberTrees * (1/(int(species.MaxStandDensity) * regionArea))
+            
+        #print(self.Name + ": " + str(result))
+        return result
+
+    #GSO for the region according to user guide formula. Also extracting species GSOs here for graphs
+    def GSO(self, step):
+        result = 0
+        for s in self.speciesArray:
+            grow = s.GrowSpace(step)
+            result += grow
+            
+        return result
+
+class Species: 
     #all attributes are from SpeciesAttrInput file in the user guide
     def __init__(self, name, long, matur, shade, fire, effd, MaxD, veg_p, MinVp, MaxVp, rcls, spt1, spt2, MaxDbh, MaxSdi, totseed, carbonco):
         self.Name = name
@@ -43,48 +86,12 @@ class Species:
         self.MaxStandDensity = MaxSdi
         self.NumberSeeds = totseed
         self.CarbonCoeffiecent = carbonco
-
-    #Calculates The Number of Trees in a diameter group (currently using the age cohort to group since DBH is determined by age)
-    def NumberTreesInDiameterGroup(self, age, timestep):
-        num = 0;
-        for tree in self.trees:
-            if tree.AgeCohort == age:
-                num += 1
-
-        return self.DBH(age, timestep), num #returns the DBH of the group along with the number of trees in the group
-
-    #GSO for a species according to user guide formula
-    def GrowSpace(self, timestep):
-        result = 0
-        for i in range(1, int(self.Longevity)//timestep):
-            dbh, numberTrees = self.NumberTreesInDiameterGroup(i, timestep)
-            result += ((dbh / 10) ** 1.605) * numberTrees * (1/(int(self.MaxStandDensity) * regionArea))
-            
-        #print(self.Name + ": " + str(result))
-        return result
-
-    #DBH for species according to user guide formula
-    def DBH(self, ageCohort, timestep):
-        age = ageCohort * timestep
-        #constants of the DBH equasion. User guided didnt have any values or say where they came froms so these are placeholder values
-        a = 1 
-        k = 1.5
-        #end constants
-        return int(self.MaxDiameter) * math.exp(-a * math.exp(-k * (age / int(self.Longevity))))
         
 
 class Tree:
     def __init__(self, species, age):
         self.Species = species
         self.AgeCohort = age
-    
-#generates a some what randomized tree set for each species to test simulation
-def GenerateTrees():
-    for s in speciesArray:
-        numTree = random.randint(500, 900)
-        values.append(numTree)
-        for i in range(numTree):
-            s.trees.append(Tree(s, random.randint(0, int(s.Longevity)//timeInc)))
 
 #checks if string is a number
 def is_number(s):
@@ -133,17 +140,6 @@ def LoadFile():
     finally:
         f.close()
     
-
-#GSO for the region according to user guide formula. Also extracting species GSOs here for graphs
-def GSO(step):
-    result = 0
-    for s in speciesArray:
-        grow = s.GrowSpace(step)
-        GSOValues.append(grow)
-        result += grow
-
-    GSOValues.append(result)
-    return result
 
 LoadFile()
 GenerateTrees()
