61a50155f66300f997212207a1a95bd74e579d62
TNice <tylernicewarner@yahoo.com>
2019-03-14 11:11:18 -0500

    Maps fixing
    fixed rendering fucked up overflow

--- JavaScript/background.js
+++ JavaScript/background.js
@@ -1,46 +1,42 @@
-var map;
+ // don't forget to add gmaps-heatmap.js
+ var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
+ // map options,
+ var myOptions = {
+   zoom: 3,
+   center: myLatlng
+ };
+ // standard map
+ map = new google.maps.Map(document.getElementById("map"), myOptions);
+ // heatmap layer
+ heatmap = new HeatmapOverlay(map, {
+   // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+   "radius": 15,
+   "maxOpacity": 1,
+   // scales the radius based on map zoom
+   "scaleRadius": false,
+   // if set to false the heatmap uses the global maximum for colorization
+   // if activated: uses the data maximum within the current map boundaries 
+   //   (there will always be a red spot with useLocalExtremas true)
+   "useLocalExtrema": true,
+   // which field name in your data represents the latitude - default "lat"
+   latField: 'lat',
+   // which field name in your data represents the longitude - default "lng"
+   lngField: 'lng',
+   // which field name in your data represents the data value - default "value"
+   valueField: 'count'
+ });
 
-function loadMap(){
-    
-    // don't forget to add gmaps-heatmap.js
-var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
-// map options,
-var myOptions = {
-  zoom: 3,
-  center: myLatlng
-};
-// standard map
-map = new google.maps.Map(document.getElementById("map"), myOptions);
-// heatmap layer
-heatmap = new HeatmapOverlay(map, 
-  {
-    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-    "radius": 15,
-    "maxOpacity": 1, 
-    // scales the radius based on map zoom
-    "scaleRadius": false, 
-    // if set to false the heatmap uses the global maximum for colorization
-    // if activated: uses the data maximum within the current map boundaries 
-    //   (there will always be a red spot with useLocalExtremas true)
-    "useLocalExtrema": true,
-    // which field name in your data represents the latitude - default "lat"
-    latField: 'lat',
-    // which field name in your data represents the longitude - default "lng"
-    lngField: 'lng',
-    // which field name in your data represents the data value - default "value"
-    valueField: 'count'
-  }
-);
+ var testData = {
+   max: 8,
+   data: [{
+     lat: 24.6408,
+     lng: 46.7728,
+     count: 3
+   }, {
+     lat: 50.75,
+     lng: -1.55,
+     count: 1
+   }]
+ };
 
-var testData = {
-  max: 8,
-  data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}]
-};
-
-heatmap.setData(testData);
-}
-
-google.maps.event.addDomListener(window, "load", loadMap);
-
-
-
+ heatmap.setData(testData);
