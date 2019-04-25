30ad1f06a7ccfe488dfa49105e7d0773b0758001
TNice <tylernicewarner@yahoo.com>
2019-04-25 10:18:36 -0500

    overlay started

--- global.R
+++ global.R
@@ -9,21 +9,22 @@ library(shinyjs)
 shiny::addResourcePath("shinyjs", system.file("srcjs", package = "shinyjs"))
 
 print("Server Starting")
-cells <- geojsonio::geojson_read("testdata/us.json", what = "sp")
-cells <- ms_simplify(cells, keep = 0.01)
+# cells <- geojsonio::geojson_read("testdata/us.json", what = "sp")
+# cells <- ms_simplify(cells, keep = 0.01)
 
 pal <- colorNumeric("Greens", 0:1)
 
-mypop <- paste0(cells$Value)
+# mypop <- paste0(cells$Value)
 
-m <- leaflet(data = cells) %>% 
+m <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>% 
   addTiles() %>% 
-  addPolygons(
-    stroke = FALSE, 
-    fillOpacity = 0.7, 
-    fillColor = ~pal(Value), 
-    popup = mypop,
-    layerId = ~Id) %>%
-  setView(lng = -98.583, lat = 39.833, zoom = 5)
+  # addPolygons(
+  #   stroke = FALSE, 
+  #   fillOpacity = 0.7, 
+  #   fillColor = ~pal(Value), 
+  #   popup = mypop,
+  #   layerId = ~Id) %>%
+  setView(lng = -98.583, lat = 39.833, zoom = 5) %>%
+  
 
 print("Server Ready")
--- ui.R
+++ ui.R
@@ -1,6 +1,16 @@
 ui <- fluidPage(
   tags$style(type = "text/css", "#map {height: 100vh !important; width: 100vw !important;} .container-fluid{padding:0px;}"),
   leafletOutput("map"),
-  actionButton("button", "An action button")
-  #htmlTemplate("./www/index.html")
+  absolutePanel(
+    top = 0,
+    bottom = 0,
+    right = 0,
+    left = 0,
+    sidebarLayout(
+      titlePanel(""),
+      sidebarPanel(
+        selectInput("dataChoice", choices = c("test1", "test2"), label = "Simulation Data")
+      )
+    )
+  )
 )
