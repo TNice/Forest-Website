9dd27925d87dfce921f180514d17f8a0a62f241c
Austin Hales <pjh539@mail.missouri.edu>
2019-04-25 10:49:30 -0500

    Added json folder

--- global.R
+++ global.R
@@ -9,21 +9,21 @@ library(shinyjs)
 shiny::addResourcePath("shinyjs", system.file("srcjs", package = "shinyjs"))
 
 print("Server Starting")
-# cells <- geojsonio::geojson_read("testdata/us.json", what = "sp")
-# cells <- ms_simplify(cells, keep = 0.01)
+cells <- geojsonio::geojson_read("testdata/USCounties.json", what = "sp")
+cells <- ms_simplify(cells, keep = 0.01)
 
 pal <- colorNumeric("Greens", 0:1)
 
 # mypop <- paste0(cells$Value)
 
-m <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>% 
+m <- leaflet(options = leafletOptions(zoomControl = FALSE), data = cells) %>% 
   addTiles() %>% 
-  # addPolygons(
-  #   stroke = FALSE, 
-  #   fillOpacity = 0.7, 
-  #   fillColor = ~pal(Value), 
-  #   popup = mypop,
-  #   layerId = ~Id) %>%
+  addPolygons(
+    stroke = FALSE,
+    fillOpacity = 0.7,
+    fillColor = ~pal(CENSUSAREA),
+    popup = mypop,
+    layerId = ~Id) %>%
   setView(lng = -98.583, lat = 39.833, zoom = 5) %>%
   
 
