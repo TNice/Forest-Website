767e153cb36a6de63829f3a131edda652e8c87dc
Austin Hales <pjh539@mail.missouri.edu>
2019-04-30 13:14:12 -0500

    Can grab cell id's when drawing

--- global.R
+++ global.R
@@ -17,6 +17,48 @@ Growthpal <- colorNumeric("Greens", 0:1)
 Firepal <- colorNumeric("Reds", 0:1)
 Insectpal <- colorNumeric("Blues", 0:1)
 
+compareCells <- function(cords){
+
+  print(cells$Id[1])
+  cellArray <- c()
+  minLat <- cords[[1]][[1]]
+  minLong <- cords[[1]][[2]]
+  maxLat <-cords[[2]][[1]]
+  maxLong <- cords[[2]][[2]]
+  counter <- 0
+  print(cells@polygons[[1]]@labpt)
+  
+  print("Test")
+  print(minLat)
+  
+  for(C in cells@polygons){
+    counter <- counter + 1
+    myCords <- C@Polygons[[1]]@coords
+    for(P in 1:nrow(myCords)){
+      if(minLat < myCords[P,1] && myCords[P,1] < maxLat && minLong < myCords[P, 2] && myCords[P,2] < maxLong){
+        
+              append(cellArray, cells$Id[counter])
+              print(cells$Id[counter])
+              break;
+      }
+    }
+  }
+
+
+  # cellArray <- c()
+  # print(cells, 1:5)
+  # for (F in cells$Polygons){
+  #   for(point in F$Polygon$coords){
+  #     if(minLat < point[0] && point[0] < maxLat && minLong < points[1] && points[1] < maxLong){
+  #       append(cellArray, F$Id)
+  #       print(F$Id)
+  #       break;
+  #     }
+  #   }
+  # }
+  # return(cellArray)
+}
+
 m <- leaflet(options = leafletOptions(zoomControl = FALSE), data = cells) %>%
   addDrawToolbar(
     targetGroup='draw',
